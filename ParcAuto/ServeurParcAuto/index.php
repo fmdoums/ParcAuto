27133be58a5cf5a482ac1370a63d41df093c0965
Faciqui Doumbia <faciqui@Faciquis-MacBook-Air.local>
2021-03-25 13:11:18 +0100

    Création du serveur et du système de routage

--- ParcAuto/ServeurParcAuto/index.php
+++ ParcAuto/ServeurParcAuto/index.php
@@ -10,7 +10,20 @@
             if (empty($url[0]) || empty($url[1])) throw new Exception("La page n'existe pas");
             switch ($url[0]) {
                 case 'front':
-                    echo "page front demandée";
+                    switch ($url[1]) {
+                        case 'voitures':
+                            echo "données JSON des voitures demandées";
+                            break;
+                        case 'voiture':
+                            echo "données JSON de la voiture ". $url[2]. " demandée";
+                            break;
+                        case 'categorie':
+                            echo "données JSON des catégorie demandée";
+                            break;
+                        default:
+                            throw new Exception("La page n'existe pas");
+                            break;
+                    }
                     break;
                 case 'back':
                     echo "page back demandée";
